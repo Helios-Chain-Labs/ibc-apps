8864d2772a80f4a5f848c1bed26c52159885e4a3
jguyet <jguyet.weoohh@gmail.com>
2024-11-18 09:57:45 +0100

    Publish v0.50.4-helios-1

--- publish.sh
+++ publish.sh
@@ -1,4 +1,4 @@
-VERSION=${VERSION:-"v8.50.3-helios-1"}
+VERSION=${VERSION:-"v0.50.4-helios-1"}
 
 echo "Deploy ibc-apps/modules/ibc-hooks/v8"
 git add .
@@ -7,6 +7,6 @@ git push
 git tag ibc-apps/modules/ibc-hooks/$VERSION
 git push origin ibc-apps/modules/ibc-hooks/$VERSION
 cd ./modules/ibc-hooks
-GOPROXY=proxy.golang.org go list -m github.com/Helios-Chain-Labs/ibc-apps/modules/ibc-hooks/v8@$VERSION
+GOPROXY=proxy.golang.org go list -m github.com/Helios-Chain-Labs/ibc-apps/modules/ibc-hooks@$VERSION
 
 echo "Publish done"
